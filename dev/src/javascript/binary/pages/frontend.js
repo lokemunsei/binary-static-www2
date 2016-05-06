f3acaab6095499ba762387577610073ce6ffc868
System Administrator <root@Lokes-MacBook-Pro.local>
2016-05-06 12:52:52 +0800

    Auto-generated commit

--- dev/src/javascript/binary/pages/frontend.js
+++ dev/src/javascript/binary/pages/frontend.js
@@ -552,6 +552,9 @@ function isNotBackoffice() {
 pjax_config_page('/\?.+|/home', function() {
     return {
         onLoad: function() {
+            if(/^(\/|\/home)$/i.test(window.location.pathname)) {
+                page.client.redirect_if_login();
+            }
             check_login_hide_signup();
             submit_email();
         }
@@ -680,11 +683,11 @@ pjax_config_page('/terms-and-conditions', function() {
     };
 });
 
-pjax_config_page('/login', function() {
+pjax_config_page('\/login|\/loginid_switch', function() {
     return {
         onLoad: function() {
-            if (page.user.is_logged_in) {
-              window.location.href = page.url.url_for('user/my_accountws');
+            if(isNotBackoffice()) {
+                window.location.href = page.url.url_for('oauth2/authorize', 'app_id=binarycom');
             }
         }
     };
